66aa5d7e122412719139ab6c422e62a09d9838b4
Dina Berry <dinaberry@outlook.com>
2021-12-19 15:24:04 -0800

    lowercase login route

--- client/src/Components/NavigationBar.tsx
+++ client/src/Components/NavigationBar.tsx
@@ -16,7 +16,7 @@ const NavigationBar: React.FC<Props> = ({ title }) => {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/Login">Login</Nav.Link>
+                            <Nav.Link href="/login">Login</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
