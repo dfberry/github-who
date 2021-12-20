19da55af36080c43aeb3be096c60c45496016e64
Dina Berry <dinaberry@outlook.com>
2021-12-20 15:21:20 -0800

    again

--- api/oauth-token/index.ts
+++ api/oauth-token/index.ts
@@ -1,6 +1,6 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import fetch from 'cross-fetch';
-import { getEnvironment  } from '../shared/environment';
+//import { getEnvironment  } from '../shared/environment';
 
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
@@ -8,19 +8,17 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('APILOG: api/github/oauth/access_token');
     const code = (req.query.code || (req.body && req.body.code));
 
-    const environment = getEnvironment(); 
-
-    const body = {
-      "client_id": environment.gitHubClientId,
-      "client_secret": environment.gitHubClientSecret,
-      "redirect_uri": environment.gitHubRedirectUri,
-      "code": code
-    };
+    //const environment = getEnvironment(); 
 
     // Request to exchange code for an access token
     const responseToken = await fetch(`https://github.com/login/oauth/access_token`, {
       method: "POST",
-      body: JSON.stringify(body),
+      body: JSON.stringify({
+        "client_id": process.env.GITHUB_OAUTH_CLIENT_ID,
+        "client_secret": process.env.GITHUB_OAUTH_CLIENT_SECRET,
+        "redirect_uri": process.env.GITHUB_OAUTH_REDIRECT_URI,
+        "code": code
+      }),
       headers: {
         "Content-Type": "application/json",
         "Accept": "application/json"
@@ -28,6 +26,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     });
     const tokenObj = await responseToken.json();
 
+    /*
     const responseUser = await fetch(`https://api.github.com/user`, {
         headers: {
           "Authorization": `token ${tokenObj.access_token}`,
@@ -36,11 +35,11 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         }
       });
     const userObj = await responseUser.json();
-  
+      */
 
     context.res = {
         status: 200,
-        body: {token: tokenObj, user: userObj}
+        body: {token: tokenObj}
     };
 };
 
