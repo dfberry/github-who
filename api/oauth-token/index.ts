8d0ce5d09e5cfc7171a703bc66cfae297f1316eb
Dina Berry <dinaberry@outlook.com>
2021-12-20 15:14:13 -0800

    ripped apart token api

--- api/oauth-token/index.ts
+++ api/oauth-token/index.ts
@@ -1,6 +1,6 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { requestTokenFromGitHub } from '../shared/github';
-import { logInit, trace } from '../shared/logging';
+import fetch from 'cross-fetch';
+import { getEnvironment  } from '../shared/environment';
 
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
@@ -8,10 +8,39 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('APILOG: api/github/oauth/access_token');
     const code = (req.query.code || (req.body && req.body.code));
 
-    let responseMessage = await requestTokenFromGitHub(code);
+    const environment = getEnvironment(); 
+
+    const body = {
+      "client_id": environment.gitHubClientId,
+      "client_secret": environment.gitHubClientSecret,
+      "redirect_uri": environment.gitHubRedirectUri,
+      "code": code
+    };
+
+    // Request to exchange code for an access token
+    const responseToken = await fetch(`https://github.com/login/oauth/access_token`, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    });
+    const tokenObj = await responseToken.json();
+
+    const responseUser = await fetch(`https://api.github.com/user`, {
+        headers: {
+          "Authorization": `token ${tokenObj.access_token}`,
+          "Accept": 'application/json',
+          "Content-Type": "application/json",     
+        }
+      });
+    const userObj = await responseUser.json();
+  
+
     context.res = {
         status: 200,
-        body: responseMessage
+        body: {token: tokenObj, user: userObj}
     };
 };
 
