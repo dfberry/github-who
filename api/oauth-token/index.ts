3ba599046908c3d5945bd80534df986163320aac
Dina Berry <dinaberry@outlook.com>
2021-12-21 09:04:24 -0800

    api catch error and return it

--- api/oauth-token/index.ts
+++ api/oauth-token/index.ts
@@ -4,21 +4,24 @@ import fetch from 'cross-fetch';
 
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-        
+
+  try {
+
     context.log('APILOG: api/github/oauth/access_token');
     const code = (req.query.code || (req.body && req.body.code));
 
     //const environment = getEnvironment(); 
+    const gitHubBody = {
+      "client_id": process.env.GITHUB_OAUTH_CLIENT_ID,
+      "client_secret": process.env.GITHUB_OAUTH_CLIENT_SECRET,
+      "redirect_uri": process.env.GITHUB_OAUTH_REDIRECT_URI,
+      "code": code
+    }
 
     // Request to exchange code for an access token
     const responseToken = await fetch(`https://github.com/login/oauth/access_token`, {
       method: "POST",
-      body: JSON.stringify({
-        "client_id": process.env.GITHUB_OAUTH_CLIENT_ID,
-        "client_secret": process.env.GITHUB_OAUTH_CLIENT_SECRET,
-        "redirect_uri": process.env.GITHUB_OAUTH_REDIRECT_URI,
-        "code": code
-      }),
+      body: JSON.stringify(gitHubBody),
       headers: {
         "Content-Type": "application/json",
         "Accept": "application/json"
@@ -26,21 +29,33 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     });
     const tokenObj = await responseToken.json();
 
-    
+
     const responseUser = await fetch(`https://api.github.com/user`, {
-        headers: {
-          "Authorization": `token ${tokenObj.access_token}`,
-          "Accept": 'application/json',
-          "Content-Type": "application/json",     
-        }
-      });
+      headers: {
+        "Authorization": `token ${tokenObj.access_token}`,
+        "Accept": 'application/json',
+        "Content-Type": "application/json",
+      }
+    });
     const userObj = await responseUser.json();
-      
+
 
     context.res = {
-        status: 200,
-        body: {token: tokenObj, user: userObj}
+      status: 200,
+      body: { token: tokenObj, user: userObj },
+      headers: {
+        "Content-Type": "application/json",
+      }
     };
+  } catch (err) {
+    context.res = {
+      status: 500,
+      body: { error: err.message, stack: err.stack, code: err.code },
+      headers: {
+        "Content-Type": "application/json",
+      }
+    };
+  }
 };
 
 export default httpTrigger;
