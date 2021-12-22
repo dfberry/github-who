7c26d50bd1031ff3f13da2a498c3a1ac9316813b
Dina Berry <dinaberry@outlook.com>
2021-12-22 05:46:42 -0800

    log env var

--- client/src/features/environment/environmentSlice.ts
+++ client/src/features/environment/environmentSlice.ts
@@ -9,20 +9,25 @@ const initialState = (): Environment => {
     const isProduction:boolean = (process.env.NODE_ENV && (process.env.NODE_ENV.toLowerCase().includes('prod')))
         ? true
         : false;
+    console.log(`isProduction ${isProduction.toString()}`);
 
     // empty redirect means SWA proxies to API
     const gitHubRedirectUri: string = (isProduction) 
         ? ""
         : (process.env.REACT_APP_GITHUB_REDIRECT_URI) || "";
+    console.log(gitHubRedirectUri);
 
     // Required
     const gitHubClientId = process.env.REACT_APP_GITHUB_OAUTH_CLIENT_ID;
+    console.log(gitHubClientId);
 
     // Required
     const gitHubState = process.env.REACT_APP_GITHUB_STATE;
+    console.log(gitHubState);
 
     // Required
     const apiBaseUri = process.env.REACT_API_BASE_URL;
+    console.log(apiBaseUri);
 
     if(!isProduction && !gitHubRedirectUri){
         throw new Error("State: expect development redirect but didn't find one");
