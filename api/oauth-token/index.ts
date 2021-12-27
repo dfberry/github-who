01165c2c20d0c6fbe7c19b1e5234ef0be1184616
Dina Berry <dinaberry@outlook.com>
2021-12-27 15:45:29 -0800

    add api

---
+++ api/oauth-token/index.ts
@@ -0,0 +1,121 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import fetch from 'cross-fetch';
+//import { getEnvironment  } from '../shared/environment';
+let appInsights = require('applicationinsights');
+
+// Get key from env APPINSIGHTS_INSTRUMENTATIONKEY
+appInsights
+  .setup()
+  .setAutoDependencyCorrelation(true)
+  .setAutoCollectRequests(true)
+  .setAutoCollectPerformance(true, true)
+  .setAutoCollectExceptions(true)
+  .setAutoCollectDependencies(true)
+  .setAutoCollectConsole(true, true)
+  .setUseDiskRetryCaching(true)
+  .setSendLiveMetrics(false)
+  .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
+  .start();
+
+let client = appInsights.defaultClient;
+
+// `code` param is reserved for Azure Function auth access
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    client.trackTrace({
+      message: 'client.track APILOG: api/github/oauth/access_token'
+    });
+    context.log('context.log APILOG: api/github/oauth/access_token');
+    const githubcode =
+      req.query.githubcode || (req.body && req.body.githubcode);
+
+    if (!githubcode)
+      throw new Error("Token: code is required but wasn't found");
+
+    //const environment = getEnvironment();
+    const gitHubBody = {
+      client_id: process.env.GITHUB_OAUTH_CLIENT_ID,
+      client_secret: process.env.GITHUB_OAUTH_CLIENT_SECRET,
+      redirect_uri: process.env.GITHUB_OAUTH_REDIRECT_URI,
+      code: githubcode
+    };
+
+    if (!process.env.GITHUB_OAUTH_CLIENT_ID)
+      throw new Error(
+        "Token: process.env.GITHUB_OAUTH_CLIENT_ID is required but wasn't found"
+      );
+    if (!process.env.GITHUB_OAUTH_CLIENT_SECRET)
+      throw new Error(
+        "Token: process.env.GITHUB_OAUTH_CLIENT_SECRET is required but wasn't found"
+      );
+    if (!process.env.GITHUB_OAUTH_REDIRECT_URI)
+      throw new Error(
+        "Token: process.env.GITHUB_OAUTH_REDIRECT_URI is required but wasn't found"
+      );
+
+    // Request to exchange code for an access token
+    const responseToken = await fetch(
+      `https://github.com/login/oauth/access_token`,
+      {
+        method: 'POST',
+        body: JSON.stringify(gitHubBody),
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        }
+      }
+    );
+    const tokenObj = await responseToken.json();
+
+    const responseUser = await fetch(`https://api.github.com/user`, {
+      headers: {
+        Authorization: `token ${tokenObj.access_token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+    const userObj = await responseUser.json();
+
+    context.res = {
+      status: 200,
+      body: {
+        token: tokenObj,
+        user: userObj
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+  } catch (err) {
+    if (err.message.includes('Token:')) {
+      context.res = {
+        status: 404,
+        body: {
+          error: err.message,
+          stack: err.stack,
+          code: err.code
+        },
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+    } else {
+      context.res = {
+        status: 500,
+        body: {
+          error: 'API: ' + err.message,
+          stack: err.stack,
+          code: err.code
+        },
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+    }
+  }
+};
+
+export default httpTrigger;
---
+++ api/status/index.ts
@@ -0,0 +1,42 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { logInit, trace } from '../shared/logging';
+
+import * as appInsights from 'applicationinsights';
+
+appInsights
+  .setup(process.env.AZURE_APPLICATIONINSIGHTS_INSTRUMENTATION_KEY)
+  .setAutoDependencyCorrelation(true)
+  .setAutoCollectRequests(true)
+  .setAutoCollectPerformance(true, true)
+  .setAutoCollectExceptions(true)
+  .setAutoCollectDependencies(true)
+  .setAutoCollectConsole(true)
+  .setUseDiskRetryCaching(true)
+  .setSendLiveMetrics(false)
+  .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
+  .start();
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    context.log('/api/status');
+
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: process.env
+    };
+  } catch (err) {
+    const returnError = {
+      error: JSON.stringify(err)
+    };
+
+    context.res = {
+      status: 500 /* Defaults to 200 */,
+      body: returnError
+    };
+  }
+};
+
+export default httpTrigger;
