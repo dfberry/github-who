46787764e87c9a7fc738618d6d29b49f4cc1538d
“Dina <dinaberry@outlook.com>
2021-12-29 12:30:38 -0800

    basic playwright test on deployed test site

--- e2e/tests/basic.spec.ts
+++ e2e/tests/basic.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
-  await page.goto('https://thankful-glacier-0c994031e.azurestaticapps.net/');
+  await page.goto('https://mango-mushroom-00bed2310.azurestaticapps.net/');
   const title = page.locator('.Home');
   await expect(title).toHaveText('Home View 1Lorem ipsum dolor sit amet, consectetur adip.');
 });
